1abd7459d23b9b0ecac8568577fbc72b77e335bc
JaeHyukYoon <gu9062@naver.com>
2022-10-23 10:39:34 +0900

    fifth commit

--- matrix/cppmatrix.cpp
+++ matrix/cppmatrix.cpp
@@ -1,51 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+using std::endl;
+using std::cin;
 
 main()
 {
 	int rowlen;	//행 길이(세로)
 	int collen;	//열 길이(가로)
+	int num;
 	
-	printf("Enter matrix (row col) size : ");	
-	scanf("%d %d", &rowlen, &collen);	//행 열 사이즈 입력받기
+	cout << "Enter matrix (row col) size : ";
+	cin >> rowlen;
+	cin >> collen;
+
+	vector<vector<int>> matrix1;
+	matrix1.reserve(rowlen);	//입력받은 행 크기만큼 메모리 할당
 	
-	//첫번째 행열
-	int** matrix1 = malloc(sizeof(int*) * rowlen);	//(int의 size)*행 크기만큼 메모리 동적 할당
+	vector <int> m1;
+	m1.reserve(collen);		//입력받은 열 크기만큼 메모리 할당
 	
-	for (int i = 0; i < rowlen; i++)
-	{
-		matrix1[i] = malloc(sizeof(int) * collen);	//각 행마다 (int의 size)*열 크기만큼  메모리 동적 할당
-	}
-	
-	//두번째 행열
-	int** matrix2 = malloc(sizeof(int*) * rowlen);	//(int의 size)*행 크기만큼 메모리 동적 할당
-	
-	for (int i = 0; i < rowlen; i++)
-	{
-		matrix2[i] = malloc(sizeof(int) * collen);	//각 행마다 (int의 size)*열 크기만큼  메모리 동적 할당
-	}
+	matrix1.push_back(m1);		//matrix1 벡터안에 m1벡터를 넣어 행렬 생성
 	
 	
-	//두 행열의 덧셈결과값 저장할 행열
-	int** resultmatrix = malloc(sizeof(int*) * rowlen);	//(int의 size)*행 크기만큼 메모리 동적 할당
+	vector<vector<int>> matrix2;
+	matrix2.reserve(rowlen);	//입력받은 행 크기만큼 메모리 할당
 	
-	for (int i = 0; i < rowlen; i++)
-	{
-		resultmatrix[i] = malloc(sizeof(int) * collen);   //각 행마다 (int의 size)*열 크기만큼  메모리 동적 할당
-	}
+	vector <int> m2;
+	m1.reserve(collen);		//입력받은 열 크기만큼 메모리 할당
+	
+	matrix2.push_back(m2);		//matrix2 벡터안에 m2벡터를 넣어 행렬 생성
 	
 	
-	if(matrix1 == NULL || matrix2 == NULL || resultmatrix == NULL) { //메모리 할당 안되었다면 실패 메세지 출력과 실행 종료
-		printf("memory setting failed\n");
+	vector<vector<int>> resultmatrix;
+	resultmatrix.reserve(rowlen);	//입력받은 행 크기만큼 메모리 할당
+	
+	vector <int> m3;
+	m3.reserve(collen);		//입력받은 열 크기만큼 메모리 할당
+	
+	resultmatrix.push_back(m3);		//matrix1 벡터안에 m1벡터를 넣어 행렬 생성
+	
+	
+	
+	if(matrix1.empty() || matrix2.empty() || resultmatrix.empty()) { //메모리 할당 안되었다면 실패 메세지 출력과 실행 종료
+		cout << "memory setting failed\n";
 		exit(1);
 	}
 	
+	
 	for(int i = 0; i < rowlen; i++)
 	{
 		for(int j = 0; j < collen; j++)
 		{
-			printf("Enter first matrix[%d][%d] value = ", i, j);	
-			scanf("%d", &matrix1[i][j]);			//첫번째 행렬에 값 입력 받기
+			num = 0;
+			cout << "Enter first matrix[" << i << "][" << j << "] value = ";
+			cin >> num;				//첫번째 행렬에 값 입력 받기	
+			matrix1[i].push_back(num);		//matrix1[i]에 담겨있는 m1벡터에 num입력
 		}
 	}
 	
@@ -53,8 +65,10 @@ main()
 	{
 		for(int j = 0; j < collen; j++)
 		{
-			printf("Enter second matrix[%d][%d] value = ", i, j);
-			scanf("%d", &matrix2[i][j]);			//두번째 행렬에 값 입력 받기
+			num = 0;
+			cout << "Enter second matrix[" << i << "][" << j << "] value = ";
+			cin >> num;				//두번째 행렬에 값 입력 받기	
+			matrix2[i].push_back(num);		//matrix2[i]에 담겨있는 m2벡터에 num입력
 		}
 	}
 	
@@ -62,7 +76,7 @@ main()
 	{
 		for(int j = 0; j < collen; j++)
 		{
-			resultmatrix[i][j] = matrix1[i][j] + matrix2[i][j]; //두행렬 덧셈 결과값 결과행렬에 저장
+			resultmatrix[i].push_back(matrix1[i][j] + matrix2[i][j]); //두행렬 덧셈 결과값 결과행렬에 저장
 		}
 	}
 	
@@ -70,21 +84,9 @@ main()
 	{
 		for(int j = 0; j < collen; j++)
 		{
-			printf("resultmatrix[%d][%d] = %d ", i, j, resultmatrix[i][j]);
+			cout << "resultmatrix[" << i << "][" << j << "] = " << resultmatrix[i][j] << " ";
 		}
-		printf("\n");
+		cout << endl;
 	}
-	
-	for(int i = 0; i < rowlen; i++)
-	{
-		free(matrix1[i]);		//열(가로공간) 메모리 반환
-		free(matrix2[i]);
-		free(resultmatrix[i]);
-	}
-	
-	free(matrix1);			//행(세로공간) 메모리 반환
-	free(matrix2);
-	free(resultmatrix);
-
 }
 
